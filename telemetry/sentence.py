5198da0e9055079de938a7d695a4ef0d13c95c85
Jonathan Barton (MHE) <jonathan.barton@mheducation.com>
2016-07-29 23:36:34 -0400

    adds dms to dd conversion

--- telemetry/sentence.py
+++ telemetry/sentence.py
@@ -1,4 +1,4 @@
-import time
+import time, re
 
 """
 $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
@@ -36,10 +36,15 @@ class Sentence():
 			return self.setEmptyData()
 		self.type = data[0] or ''
 		self.timestamp = data[1] or str(int(time.time()))
+
 		self.latitude = data[2] or 0
 		self.latitude_direction = data[3] or ''
-		self.longitude = data[4] or 0
+		self.latitude = self.dms2dec(self.latitude, self.latitude_direction)
+
+        self.longitude = data[4] or 0
 		self.longitude_direction = data[5] or ''
+        self.longitude = self.dms2dec(self.longitude, self.longitude_direction)
+
 		self.fix_quality = data[6] or 0
 		self.satellites = data[7] or 0
 		self.dilution = data[8] or 0
@@ -59,3 +64,19 @@ class Sentence():
 		self.satellites = '0'
 		self.dilution = '0'
 		self.altitude = '0'
+    def dms2dec(dmsString):
+        dmsString = re.sub(r'\s','',dmsString)
+        (dmsDegreesMins, dmsSeconds) = re.split('\.',dmsString, maxsplit=1)
+        if(len(dmsDegreesMins) >= 5):
+            degrees = int(dmsDegreesMins[:3])
+            minutes = int(dmsDegreesMins[3:5])
+        else:
+            degrees = int(dmsDegreesMins[:2])
+            minutes = int(dmsDegreesMins[2:4]
+        seconds = int(dmsSeconds[:2])
+        fracSeconds = int(dmsSeconds[2:])
+        sign = 1
+        if re.match('[swSW]', dmsString):
+            sign = -1
+        return str(sign * (int(degrees) + float(minutes) / 60 + float(seconds) / 3600 + float(fracSeconds) / 36000))
+        
